46d433a259c4319e2f19ea552f44d7a5fd913176
PDochev <plamendochev0@gmail.com>
2024-03-09 21:25:26 +0000

    Add ExpressError and NotFound components

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const mongoose = require("mongoose");
+const ExpressError = require("./utils/ExpressError.js");
 const documentsRoute = require("./routes/documentsRoute.js");
 const googleAuthRoute = require("./routes/googleAPI.js");
 const userRoute = require("./routes/userRoute.js");
@@ -28,16 +29,8 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000,
   })
 );
-// app.use(cors());
+
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-//Allow custom origins
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(userRoute);
@@ -47,6 +40,14 @@ app.use("/documents", documentsRoute);
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to MERN tutorial");
+});
+
+// app.all("*", (req, res, next) => {
+//   next(new ExpressError("Page Not Found", 404));
+// });
+
+app.use((req, res, next) => {
+  res.status(404).send("404 - Not Found");
 });
 
 mongoose
--- frontend/src/components/DocumentsTable.tsx
+++ frontend/src/components/DocumentsTable.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import AlertDialogDelete from "./AlertDialogDelete";
-import { useEffect, useState } from "react";
 
 interface DocumentsTableProps {
   document: Document[];
