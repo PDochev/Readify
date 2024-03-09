1e267d0b441186c801c0a3bf058937bee127c910
PDochev <plamendochev0@gmail.com>
2024-03-09 09:16:23 +0000

    Add AuthProvider context and update theme in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,19 +6,22 @@ import Documents from "./pages/DocumentsPage/Documents";
 import ReadifyApp from "./pages/ApplicationPage/ReadifyApp";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/documents" element={<Documents />} />
-          <Route path="/documents/:id" element={<ReadifyApp />} />
-        </Routes>
-        <Toaster />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/documents" element={<Documents />} />
+            <Route path="/documents/:id" element={<ReadifyApp />} />
+          </Routes>
+          <Toaster />
+        </AuthProvider>
       </ThemeProvider>
     </>
   );
