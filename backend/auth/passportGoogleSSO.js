db81cad4f998f4efbf45732978ba375e2d233394
PDochev <plamendochev0@gmail.com>
2024-03-17 22:32:07 +0000

    Update Google SSO callback URLs

--- backend/auth/passportGoogleSSO.js
+++ backend/auth/passportGoogleSSO.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://readify-xbps.onrender.com/auth/google/callback",
+      callbackURL: "http://localhost:3000/auth/google/callback",
       passReqToCallback: true,
     },
 
