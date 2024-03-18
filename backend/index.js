ada16fbcbe535d85432b5461d7d3920c45bf4389
PDochev <plamendochev0@gmail.com>
2024-03-18 22:38:27 +0000

    Add cookie-session for XSRF protection

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
 require("./auth/passportGoogleSSO.js");
+const cookieSession = require("cookie");
 
 const db_url = process.env.DB_URL || "mongodb://localhost:27017/readify";
 const port = process.env.PORT || 3000;
@@ -21,21 +22,33 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    domain: "https://readifyapp.netlify.app/",
-    // cookie: {
-    //   httpOnly: true,
-    //   // secure: true,
-    //   expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    //   maxAge: 1000 * 60 * 60 * 24 * 7,
-    // },
-    maxAge: 24 * 60 * 60 * 1000,
+res.setHeader(
+  "Set-Cookie",
+  cookie.serialize("XSRF-TOKEN", YOUR_OBJECT, {
+    // XSRF-TOKEN is the name of your cookie
+    sameSite: "lax", // lax is important, don't use 'strict' or 'none'
+    httpOnly: process.env.ENVIRONMENT !== "development", // must be true in production
+    path: "/",
+    secure: process.env.ENVIRONMENT !== "development", // must be true in production
+    maxAge: 60 * 60 * 24 * 7 * 52, // 1 year
+    domain: "https://readifyapp.netlify.app/", // the period before is important and intentional
   })
 );
+// app.use(
+//   session({
+//     secret: process.env.COOKIE_SECRET,
+//     resave: false,
+//     saveUninitialized: true,
+//     domain: "https://readifyapp.netlify.app/",
+//     // cookie: {
+//     //   httpOnly: true,
+//     //   // secure: true,
+//     //   expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+//     //   maxAge: 1000 * 60 * 60 * 24 * 7,
+//     // },
+//     maxAge: 24 * 60 * 60 * 1000,
+//   })
+// );
 
 app.use(
   cors({
