ea913c684e39941dbefd377c0495a6a50c291254
PDochev <plamendochev0@gmail.com>
2024-03-18 23:28:38 +0000

    Remove httpOnly flag from cookie configuration

--- backend/index.js
+++ backend/index.js
@@ -44,9 +44,9 @@ app.use(
     cookie: {
       sameSite: "none",
       secure: true,
-      httpOnly: true,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
+      domain: "https://readifyapp.netlify.app/",
     },
   })
 );
