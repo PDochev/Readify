f5f05a5cf22decdf1096f160286922e2cdf09d33
PDochev <plamendochev0@gmail.com>
2024-04-02 21:26:13 +0100

    Update sameSite attribute in cookie configuration

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.use(
     saveUninitialized: false,
     // domain: "https://readifyapp.netlify.app/",
     cookie: {
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
