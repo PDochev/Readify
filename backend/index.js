74781a7d909fd3de28006112984754a87c279d22
PDochev <plamendochev0@gmail.com>
2024-04-02 22:02:02 +0100

    Update cookie sameSite attribute to "none"

--- backend/index.js
+++ backend/index.js
@@ -42,15 +42,13 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    proxy: true,
-
     // domain: "https://readifyapp.netlify.app/",
     cookie: {
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
-      domain: ".onrender.com",
+      // domain: ".onrender.com"
     },
   })
 );
