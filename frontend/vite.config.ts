a382502b576779e8b46fd94deac01d8cdc969932
PDochev <plamendochev0@gmail.com>
2024-04-03 21:25:39 +0100

    Add proxy configuration for server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/": {
+        target: "https://readify-xbps.onrender.com",
+        changeOrigin: true,
+      },
+    },
+  },
 });
