48504db839b8d15a49ce1c5b791959ffa6cabc17
PDochev <plamendochev0@gmail.com>
2024-04-05 21:14:29 +0100

    Update session cookie settings to include httpOnly flag in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,7 @@ app.use(
     cookie: {
       sameSite: "none",
       secure: true,
+      httpOnly: false,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
       // domain: ".onrender.com"
