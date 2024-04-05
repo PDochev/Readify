99e4fc8fec4d1e1392ee27c94db741ca35e7c120
PDochev <plamendochev0@gmail.com>
2024-04-05 21:24:35 +0100

    Update session cookie settings to include httpOnly flag and remove unused proxy configuration in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,6 @@ app.use(
     cookie: {
       sameSite: "none",
       secure: true,
-      httpOnly: false,
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7,
       // domain: ".onrender.com"
