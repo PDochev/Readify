c02a3f4de40c5c5cc44d632588b681ea03c4bcef
PDochev <plamendochev0@gmail.com>
2024-04-07 10:53:07 +0100

    Update Google SSO callback URL in passportGoogleSSO.js

--- backend/auth/passportGoogleSSO.js
+++ backend/auth/passportGoogleSSO.js
@@ -9,7 +9,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
-        process.env.MOBILE_ORIGIN + "/auth/google/callback" ||
+        process.env.GOOGLE_CALLBACK_URL ||
         "http://localhost:3000/auth/google/callback",
       passReqToCallback: true,
     },
